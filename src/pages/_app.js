d5bd5de89eaa75e5b89146c97bbef6dfe309cdb0
mohammad tahourian <mhdtahourian@gmail.com>
2022-04-08 12:58:24 +0430

    optimized Imports and Module path aliases

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MainLayout from "../components/Layouts/MainLayout"
+import MainLayout from "@components/Layouts/MainLayout"
 import "../assets/styles/main.css"
 
 function App({Component, pageProps}) {
