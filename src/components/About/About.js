163b105296346aec95a4091e7d2c619da64e6084
mohammad tahourian <mhdtahourian@gmail.com>
2022-04-08 13:13:12 +0430

    dynamic route in navbar

---
+++ src/components/About/About.js
@@ -0,0 +1,7 @@
+const AboutUs = () => {
+    return (
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium et excepturi, impedit itaque iusto
+            magni nemo nihil nobis nulla odit officia optio quam quas quia ratione reprehenderit repudiandae rerum.</p>
+    )
+}
+export  default AboutUs
---
+++ src/pages/login/index.js
@@ -0,0 +1,6 @@
+import LoginComponent from "@components/Login/Login"
+
+const Login = () => {
+    return <LoginComponent/>
+}
+export default Login
