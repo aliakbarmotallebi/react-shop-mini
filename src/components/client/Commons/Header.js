fb94ad47b2619fe39adcc64ba222015ccb035fea
mohammad tahourian <mhdtahourian@gmail.com>
2022-04-28 18:38:24 +0430

    get data from context correctly

--- src/components/client/Commons/Header.js
+++ src/components/client/Commons/Header.js
@@ -1,14 +1,10 @@
 import React, { memo, useContext } from "react";
-import AuthContext from "src/context/authContext";
 import MainNav from "../Nav/MainNav";
 
 const Header = () => {
 
   return (
     <>
-      <AuthContext.Consumer>
-        {(context) => console.log(context.user?.user.mobile)}
-      </AuthContext.Consumer>
       <MainNav />
     </>
   )
