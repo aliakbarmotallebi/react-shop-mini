3ee67fe2d7e5d851c6646f4f24398b9ba8de77d5
mohammad tahourian <mhdtahourian@gmail.com>
2022-04-29 11:53:40 +0430

    working on shop page and filters

--- src/components/Shop/shop.jsx
+++ src/components/Shop/shop.jsx
@@ -9,17 +9,17 @@ export default function ShopJozi({ query }) {
   const [hasMore, setHasMore] = useState(false)
   const [skip, setSkip] = useState(0)
   const [loading, setLoading] = useState(true)
-  
-  
-  
-   
+
+
+
+
   const observe = useRef()
   const lastProductElementRef = useCallback(node => {
     if (loading) return
     if (observe.current) observe.current.disconnect()
     observe.current = new IntersectionObserver(entries => {
       if (entries[0].isIntersecting && hasMore) {
-        setSkip(skip += 15)
+        setSkip(skip += 12)
       }
     })
     if (node) observe.current.observe(node)
@@ -31,10 +31,10 @@ export default function ShopJozi({ query }) {
     AxiosInstance({
       url: "products",
       params: {
-        count: 15,
+        count: 12,
         // q : query ? query.q : '',
         // TODO : solve this 
-        skip, 
+        skip,
       }
     }).then(result => {
       setProducts([...products, ...result.data['data']])
@@ -46,19 +46,43 @@ export default function ShopJozi({ query }) {
 
   return (
     <>
-      <div className='grid grid-cols-5'>
-        {
-          products.map((product, index) => {
-            if (products.length === index + 1) {
-              return <div key={product.Id} ref={lastProductElementRef}>< ProductItem product={product} /></div>
-            } else {
-              return <div key={product.Id} ><ProductItem product={product} /></div>
-            }
 
-          })
-        }
+      <div className="grid grid-cols-4">
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia dolorum nihil eos ratione quam minus, ea fuga voluptate aspernatur, atque nobis non illum vitae pariatur amet voluptatum facere? Quisquam, possimus.
+        </div>
+        <div className='grid col-span-3 grid-cols-4'>
+          <div class="text-black m-4 col-span-4" >
+            <ol class="list-none p-0 inline-flex">
+              <li class="flex items-center pl-4">
+                <a href="#">تمام محصولات</a>
+              </li>
+              <li class="flex items-center pl-4">
+                <a href="#">محصولات ویژه</a>
+              </li>
+              <li class="flex items-center pl-4">
+                <a href="#">پربازدیدترین</a>
+              </li>
+              <li className='pl-4'>
+                <a href="#" class="text-gray-500" aria-current="page">پرفروش ترین</a>
+              </li>
+            </ol>
+          </div>
+          {
+            products.map((product, index) => {
+              if (products.length === index + 1) {
+                return <div key={product.Id} ref={lastProductElementRef}>< ProductItem product={product} /></div>
+              } else {
+                return <div key={product.Id} ><ProductItem product={product} /></div>
+              }
+
+            })
+          }
+          {loading && (<div className='col-span-4 my-4'><Loading /></div>)}
+        </div>
+
+
       </div>
-      {loading && <Loading />}
     </>
   )
 }
--- src/components/client/card/card.jsx
+++ src/components/client/card/card.jsx
@@ -3,33 +3,33 @@ import React from "react";
 import textToSlug from "@helpers/textToSlug";
 import Link from "next/link";
 
-const Card = ({product}) => {
+const Card = ({ product }) => {
     return (
         <div className="relative mx-auto w-full">
             <Link href={`/product/${product.ErpCode}/${textToSlug(product.Name)}`}
-            className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full"
+                className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full"
             >
-            <div className="shadow p-4 rounded-lg bg-white">
-                <div className="absolute top-0 left-0 mt-2 ml-2 p-4 z-20 flex justify-between">
-                    <button className="group hover:bg-transparent bg-white border-gary-500 border rounded w-7 h-7">
-                        <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        className="group-hover:bg-transparent w-4 text-gary-500 m-auto"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        >
-                        <circle cx="9" cy="21" r="1" />
-                        <circle cx="20" cy="21" r="1" />
-                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
-                        </svg>
-                    </button>
-	          </div>
+                <div className="shadow p-4 rounded-lg bg-white">
+                    <div className="absolute top-0 left-0 mt-2 ml-2 p-4 z-20 flex justify-between">
+                        <button className="group hover:bg-transparent bg-white border-gary-500 border rounded w-7 h-7">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                className="group-hover:bg-transparent w-4 text-gary-500 m-auto"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <circle cx="9" cy="21" r="1" />
+                                <circle cx="20" cy="21" r="1" />
+                                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
+                            </svg>
+                        </button>
+                    </div>
                     <div className="flex justify-center relative rounded-lg overflow-hidden h-52">
-                        <div className="relative after:w-full after:h-full after:inset-0 after:absolute after:bg-gray-800 after:opacity-10 transition-transform duration-500 transform ease-in-out hover:scale-110 w-full">
+                        <div className="relative after:w-full after:h-full after:inset-0 after:absolute  transition-transform duration-500 transform ease-in-out hover:scale-110 w-full">
                             <Image
                                 className="absolute inset-0"
                                 src={product.Image}
