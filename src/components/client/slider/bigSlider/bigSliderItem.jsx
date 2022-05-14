b295cfa318bd6b1cc23718477f6d962ed2a235ad
mohammad tahourian <mhdtahourian@gmail.com>
2022-05-14 20:29:52 +0430

    make big slider responsive and complete detail section

--- src/components/client/slider/bigSlider/bigSliderItem.jsx
+++ src/components/client/slider/bigSlider/bigSliderItem.jsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 const BigSliderItem = ({slide}) => {
     return (
-        <div className="min-w-full rounded-md">
-            <Image src={slide.Image} width={800} height={400} />
+        <div className="w-full rounded-md">
+            <Image src={slide.Image} width={800} height={400} layout={'responsive'} />
         </div>
     )
 };
