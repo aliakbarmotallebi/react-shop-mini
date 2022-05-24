56527d38c6652c02adf3e27fddf294f622780c7e
mohammad tahourian <mhdtahourian@gmail.com>
2022-05-24 13:59:40 +0430

    better customhead

--- src/components/client/header/customHead.jsx
+++ src/components/client/header/customHead.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Head from 'next/head'
 
+import Titles from '@utils/Titles'
+
 export default function CustomHead({ title }) {
     return (
         <Head>
-            <title>{title}</title>
+            <title>{`${Titles.titles.siteTitle} | ${title}`}</title>
             <link rel="icon" href="/favicon.ico" />
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"
             />
             <meta name="robots" content="index, follow" />
+            <meta name="description" content={Titles.titles.desciption} />
         </Head>
     )
 }
