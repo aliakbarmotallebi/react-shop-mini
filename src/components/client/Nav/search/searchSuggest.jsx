0f160ed367a6742e9245d6e3fd756809c160e0b1
mohammad tahourian <mhdtahourian@gmail.com>
2022-06-24 11:33:56 +0430

    remove search box after click items

--- src/components/client/Nav/search/searchSuggest.jsx
+++ src/components/client/Nav/search/searchSuggest.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import SearchSuggestItem from './searchSuggestItem'
-import Link from 'next/link'
 
-export default function SearchSuggest({ searchedItems, searchedQuery }) {
+
+export default function SearchSuggest({ searchedItems, searchedQuery, event }) {
+
+
+
     return (
-
         searchedQuery.length >= 2 ? (<div className='absolute top-11 right-0 left-0 bg-white border'>
             <ul>
                 <li className='text-sm border-b p-2 flex items-center'>
-                    <Link href=''>
-                        <a>
-                            <p>  جست و جو محصول  :
-                                {searchedQuery}
-                            </p>
-                        </a>
-                    </Link>
+                    <p>  جست و جو محصول  :
+                        {searchedQuery}
+                    </p>
                 </li>
                 {searchedItems.map(searchedItem => (
                     <SearchSuggestItem searchedItem={searchedItem} />
--- src/components/client/Nav/search/searchSuggestItem.jsx
+++ src/components/client/Nav/search/searchSuggestItem.jsx
@@ -2,32 +2,39 @@ import React from 'react'
 import Image from 'next/image'
 import ProductLinker from '@components/client/Commons/productLinker';
 import SearchContext from 'src/context/searchContext';
+import { useContext } from 'react';
 
 export default function SearchSuggestItem({ searchedItem }) {
 
+    const { removeSearchedBox } = useContext(SearchContext)
+
+    function handleSearch() {
+        removeSearchedBox()
+        document.getElementById('searchForm').reset()
+    }
+
 
     return (
-        <SearchContext.Consumer >
-            {({ removeSearchedBox }) => (
-                <li
-                    className='md:text-sm text-xs border-b p-2 flex items-center'
-                    onClick={removeSearchedBox}>
-                    <Image
-                        src={searchedItem.Image}
-                        width={35}
-                        height={35}
-                        className={'self-center'}
-                    />
-                    <ProductLinker
-                        productErpCode={searchedItem.ErpCode}
-                        productName={searchedItem.Name} >
-                        <a className='pr-2'>
-                            {searchedItem.Name}
-                        </a>
-                    </ProductLinker>
-                </li>
-            )}
-        </SearchContext.Consumer>
+
+        <li
+            className='md:text-sm text-xs border-b p-2 flex items-center'
+            onClick={handleSearch}>
+            <Image
+                src={searchedItem.Image}
+                width={35}
+                height={35}
+                className={'self-center'}
+            />
+            <ProductLinker
+                productErpCode={searchedItem.ErpCode}
+                productName={searchedItem.Name} >
+                <a className='pr-2'>
+                    {searchedItem.Name}
+                </a>
+            </ProductLinker>
+        </li>
+
+
 
     )
 }
