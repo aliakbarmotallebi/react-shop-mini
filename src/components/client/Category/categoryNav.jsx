267a196f2f484fcee46cceddaedaf28a1d8a92a5
mohammad tahourian <mhdtahourian@gmail.com>
2022-06-25 12:48:49 +0430

    still work on categories

--- src/components/client/Category/categoryNav.jsx
+++ src/components/client/Category/categoryNav.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Config from "@utils/Config";
 import { AxiosInstance } from "@utils/http";
-import CategoryNavItem from "@components/client/Category/categoryNavItem";
 
 const CategoryNav = () => {
     const [categories, setCategories] = useState([])
+    const [subcategories, setSubcategoris] = useState([])
     const fetchCategories = () => {
         const { mainCategory } = Config.services.mainCategory
         AxiosInstance.get(`${mainCategory}`)
@@ -13,18 +13,43 @@ const CategoryNav = () => {
             }).catch((err) => console.log(err));
     }
 
+    const fetchSubCategories = (categoryErpCode) => {
+        AxiosInstance.get(`https://shopjozi.ir/api/subcategories/category/${categoryErpCode}`)
+            .then((res) => {
+                setSubcategoris(res.data['data']);
+            }).catch((err) => console.log(err));
+    }
+    const mouseLeaveHandle = () => {
+        setSubcategoris([])
+    }
+
     useEffect(() => {
         fetchCategories();
     }, [])
 
     return (
-        <div className="shadow-lg lg:shadow-none border-y">
+        <div className="shadow-lg lg:shadow-none " onMouseLeave={() => mouseLeaveHandle()}>
             <ul className="flex-col lg:flex lg:flex-row lg:items-center">
                 {categories.map((category, key) => (
-                    <CategoryNavItem key={category.Id} category={category} />
+                    <li key={key} onClick={() => fetchSubCategories(category.ErpCode)} className="lg:mx-2 hidden lg:block cursor-pointer ">
+                        <span className="text-gray-600 text-xs py-3 px-1 block font-yekan-bold categoryList-item">
+                            {category.Name}
+                        </span>
+                    </li>
                 ))}
             </ul>
+            <div className=" relative">
+                <ul className="absolute top-0 left-0 right-0 bg-slate-100">
+                    {subcategories.map((subCategory, key) => (
+                        <li key={key} className="lg:mx-2 hidden lg:inline-block">
+                            <a className="text-gray-600 text-xs py-3 px-1 block font-yekan-bold"
+                                href="#">{subCategory.Name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
+
     )
 }
 export default CategoryNav
--- src/components/client/Nav/MainNav.js
+++ src/components/client/Nav/MainNav.js
@@ -102,7 +102,7 @@ const MainNav = () => {
 
                                 </div>
                             </div>
-                            {/* <CategoryNav /> */}
+                            <CategoryNav />
                         </div>
                     </nav>
                 )}
