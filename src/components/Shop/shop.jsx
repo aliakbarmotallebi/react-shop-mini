1b23d3ef9ec2d24363fe6434207d34b158330ebd
mohammad tahourian <mhdtahourian@gmail.com>
2022-06-27 19:34:10 +0430

    fixed a bug

--- src/components/Shop/shop.jsx
+++ src/components/Shop/shop.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 import { AxiosInstance } from '@utils/http'
 import ProductItem from '@components/client/Products/productItem'
@@ -8,23 +9,20 @@ import Filter from './filter/filter'
 
 export default function ShopJozi({ page, q }) {
 
-  const productsOnPage = 28
-
   const [products, setProducts] = useState([])
-  const [skip, setSkip] = useState(page && page > 1 ? productsOnPage : 0)
   const [loading, setLoading] = useState(true)
-  const [perPage, setPerPage] = useState(page || 1)
+
+  const productsOnPage = 28
+  page = page ? parseInt(page) : 1
 
   useEffect(() => {
-    console.log(Math.abs(skip * (perPage - 1)))
-
     setLoading(true)
     AxiosInstance({
       url: "products",
       params: {
         q: q ? q : '',
         count: productsOnPage,
-        skip: Math.abs(skip * (perPage - 1)),
+        skip: (page - 1) * productsOnPage,
       }
     }).then(result => {
       setProducts(result.data['data'])
@@ -41,18 +39,33 @@ export default function ShopJozi({ page, q }) {
         <div className='grid col-span-3 grid-cols-4'>
           <Filter />
           {
-            products.map((product, index) =>
-            (
-              <div key={product.Id} >
-                <ProductItem product={product} />
-              </div>)
-            )
+            loading ? <div className='col-span-4 my-4'><Loading /></div> :
+              products.map((product, index) =>
+              (
+                <div key={product.Id} >
+                  <ProductItem product={product} />
+                </div>)
+              )
           }
-          {loading && (<div className='col-span-4 my-4'><Loading /></div>)}
+          <div className='flex justify-center gap-3 w-full col-span-4 py-8'>
+
+            <Link href={q ? { pathname: 'shop', query: { q, page: parseInt(page) + 1 } } : ''}>
+              <a>
+                <span className='bg-slate-100 text-sm rounded-sm px-4 py-2'>بعدی</span>
+              </a>
+            </Link>
+
+            <Link href="">
+              <a>
+                <span className='bg-slate-100 text-sm rounded-sm px-4 py-2'> قبلی</span>
+              </a>
+            </Link>
+
+          </div>
         </div>
 
 
       </div>
-    </div>
+    </div >
   )
 }
