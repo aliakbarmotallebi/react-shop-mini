2eb7c75784722f51a8dd113885148ab32f5198f1
mohammad tahourian <mhdtahourian@gmail.com>
2022-07-17 19:28:05 +0430

    optimize main navbar list item

--- src/components/client/Nav/MainNav.js
+++ src/components/client/Nav/MainNav.js
@@ -51,7 +51,23 @@ const MainNav = () => {
                             <NavToggle handleShowNavbar={handleShowNavbar} showNavbar={showNavbar} />
                         </div>
                         <nav>
+
                             <ul className="md:flex items-center justify-between text-sm text-gray-700 pt-4 md:pt-0">
+                                {storageUser?.user ?
+                                    <li className="px-4 py-2 md:border rounded-md border-slate-200">
+                                        {storageUser?.user?.mobile}
+                                    </li>
+                                    :
+                                    <li onClick={() => handleShowNavbar()} className="md:border rounded-md border-slate-200">
+                                        <Link href={'/login'}>
+                                            <a className="inline-block no-underline hover:text-black hover:underline py-2 px-4">
+                                                ورود / ثبت نام
+                                            </a>
+                                        </Link>
+                                    </li>
+
+                                }
+
                                 <li onClick={() => handleShowNavbar()}>
                                     <Link href={'/'}>
                                         <a className="inline-block no-underline hover:text-black hover:underline py-2 px-4"
@@ -64,12 +80,24 @@ const MainNav = () => {
                                         >فروشگاه</a>
                                     </Link>
                                 </li>
-                                <li onClick={() => handleShowNavbar()}>
-                                    <Link href={'/profile'}>
-                                        <a className="inline-block no-underline hover:text-black hover:underline py-2 px-4"
-                                        >پروفایل من</a>
-                                    </Link>
-                                </li>
+                                {storageUser?.user ?
+                                    <li onClick={() => handleShowNavbar()}>
+                                        <Link href={'/profile'}>
+                                            <a className="inline-block no-underline hover:text-black hover:underline py-2 px-4"
+                                            >پروفایل من</a>
+                                        </Link>
+                                    </li>
+                                    :
+                                    ''
+                                }
+                                {storageUser?.user ?
+                                    <li
+                                        onClick={logoutuser}
+                                        className="inline-block no-underline hover:text-black hover:underline py-2 px-4 text-xs text-rose-600 cursor-pointer">
+                                        خروج از حساب کاربری
+                                    </li>
+                                    : ''}
+
                             </ul>
                         </nav>
                         <div className="px-4">
@@ -87,19 +115,7 @@ const MainNav = () => {
                     </div>
 
                     <div className="order-2 md:order-3 flex items-center" id="nav-content">
-                        {storageUser?.user ?
-                            <button onClick={logoutuser} className="inline-block no-underline hover:text-black hover:underline py-2 px-4 text-sm text-rose-600">خروج</button>
-                            : <Link href={'/login'}>
-                                <a className="pl-3 inline-block no-underline hover:text-black">
-                                    <svg className="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24"
-                                        height="24" viewBox="0 0 24 24">
-                                        <circle fill="none" cx="12" cy="7" r="3" />
-                                        <path
-                                            d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
-                                    </svg>
 
-                                </a>
-                            </Link>}
 
                         <Link href={"/cart"}>
 
